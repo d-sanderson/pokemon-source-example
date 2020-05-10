d5c563a0d2eb840716ef05791e4718a19c9226db
David <sanderdj90@gmail.com>
2020-05-10 17:26:45 -0600

    add gatsby-source-pokemons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
 module.exports = {
   siteMetadata: {
     title: `gatsby-source-pokeapi`,
-    description: `Gatsby Source Plugin https://pokeapi.co/.`,
+    description: `Example repo of usage of the gatsby-source-pokemons plugin.`,
     author: `@dsanderson_`,
   },
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    `gatsby-source-pokemons`
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require(`path`);
-const { results } = require("pokemons");
 const types = [
   "Normal",
   "Fighting",
@@ -20,35 +19,11 @@ const types = [
   "Dark",
   "Fairy",
 ];
-const uniquePokemons = Array.from(new Set(results.map(a => a.name)))
- .map(name => {
-   return results.find(a => a.name === name)
- })
 exports.sourceNodes = ({
   actions,
-  createNodeId,
-  createContentDigest,
 }) => {
   const { createPage } = actions;
   const TypeTemplate = path.resolve(`src/templates/TypeTemplate.js`);
-  try {
-    // Map over the results array, calling action.createNode on each item in the array
-   uniquePokemons.forEach((pokemon, i) => {
-     console.log(pokemon)
-      const node = {
-        ...pokemon, // We copy all of the properties from the pokemon object
-        id: createNodeId(i), // Needs to be unique
-        internal: {
-          type: "Pokemons",
-          contentDigest: createContentDigest(pokemon), // We pass in the pokemon object to make sure it's unique
-        },
-      };
-      // Create the actual data node
-      actions.createNode(node);
-    });
-  } catch (error) {
-    console.log(error);
-  }
 
   types.forEach(type => {
     let slug = type.toLowerCase();
